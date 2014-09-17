c614685c89dc0a776202230ac5d0fd9c11188a2c
Mike Purvis <mpurvis@clearpathrobotics.com>
2014-09-17 14:25:56 -0400

    Linter fix for jackal_hardware.

--- jackal_base/src/jackal_hardware.cpp
+++ jackal_base/src/jackal_hardware.cpp
@@ -90,7 +90,8 @@ void JackalHardware::copyJointsFromHardware()
  */
 void JackalHardware::publishDriveFromController()
 {
-  if (cmd_drive_pub_.trylock()){
+  if (cmd_drive_pub_.trylock())
+  {
     cmd_drive_pub_.msg_.mode = jackal_msgs::Drive::MODE_VELOCITY;
     cmd_drive_pub_.msg_.drivers[jackal_msgs::Drive::LEFT] = joints_[0].velocity_command;
     cmd_drive_pub_.msg_.drivers[jackal_msgs::Drive::RIGHT] = joints_[1].velocity_command;
