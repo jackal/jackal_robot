e2d1d6030b0e80759c29ab848e37abc6b5c66fda
Mike Purvis <mpurvis@clearpathrobotics.com>
2015-02-03 16:32:40 -0500

    Add multicast module to setup.py

--- jackal_bringup/setup.py
+++ jackal_bringup/setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup(**generate_distutils_setup(
-    packages=['jackal_bringup'],
+    packages=['jackal_bringup', 'jackal_bringup.multicast'],
     package_dir={'': 'src'}
 ))
