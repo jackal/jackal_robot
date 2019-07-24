b0d5a138002260e72fa1be174fe0ce4443c8620b
James Xu <kejia.xu@microsoft.com>
2019-07-23 18:19:25 -0700

    fix calibration script (#5)
    * fix batch file
    
    * fix batch script for mix use of forward slash
    
    * remove todo comment

--- jackal_base/scripts/calibrate_compass.bat
+++ jackal_base/scripts/calibrate_compass.bat
@@ -6,30 +6,31 @@ REM only run this script when there is no rostopic or rosbag running
 for /f "tokens=2" %%P in ('
   tasklist /fi "Imagename eq rostopic.*" /fo list ^| findstr /b "PID:"
 ') do (
-  echo Error! Please run this script without any running instance of rostopic!
+  echo Error^! Please run this script without any running instance of rostopic^!
   exit /b 1
 )
 for /f "tokens=2" %%P in ('
   tasklist /fi "Imagename eq rosbag.*" /fo list ^| findstr /b "PID:"
 ') do (
-  echo Error! Please run this script without any running instance of rosbag!
+  echo Error^! Please run this script without any running instance of rosbag^!
   exit /b 1
 )
 
-set TEMPDIR=%temp%\jackal.calibrate_compass
+set TEMPDIR="%temp%\jackal.calibrate_compass"
 if exist %TEMPDIR% (
   if exist %TEMPDIR%\nul (
     rd /s /q %TEMPDIR%
   )
 )
-set BAG_FILE=%TEMPDIR%\imu_record.bag
-set CAL_FILE=%TEMPDIR%\mag_config.yaml
-set CAL_FINAL_PATH=%ROS_ETC_DIR%\jackal_base
+mkdir %TEMPDIR%
+set BAG_FILE="%TEMPDIR%\imu_record.bag"
+set CAL_FILE="%TEMPDIR%\mag_config.yaml"
+set CAL_FINAL_PATH="%ROS_ETC_DIR%\jackal_base"
 set DURATION=60
 
 call rostopic list >nul
 if not "%errorlevel%" == "0" (
-  echo ROS appears not to be running. Please start ROS service!
+  echo ROS appears not to be running. Please start ROS service^!
   exit /b 1
 )
 
@@ -83,15 +84,17 @@ if not "%errorlevel%" == "0" (
   exit /b 1
 )
 
+(set _RESPONSE=)
 if exist %CAL_FILE% (
   echo Calibration generated in %CAL_FILE%.
+  set /p _RESPONSE=Copy calibration to %CAL_FINAL_PATH%? [Y/n] || set response=Y
+)
 
-  set /p response=Copy calibration to %CAL_FINAL_PATH%? [Y/n] || set response=Y
-
-  if %response:~0,1% == n (
+if defined _RESPONSE (
+  if "%_RESPONSE:~0,1%" == "n" (
     echo Skipping.
   ) else (
-    if %response:~0,1% == N (
+    if "%_RESPONSE:~0,1%" == "N" (
       echo Skipping.
     ) else (
       mkdir %CAL_FINAL_PATH%
